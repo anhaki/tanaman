55701386652741ab300f1ea242e9a430ca89b9aa
“anhaki” <m.anugrahhakiki@gmail.com>
2022-10-13 16:56:12 +0700

    Menambahkan file

---
+++ app/src/main/java/com/if3a/tanaman/MainActivity.java
@@ -0,0 +1,40 @@
+package com.if3a.tanaman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnlogin, btnsignup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnlogin = findViewById(R.id.btn_login);
+        btnsignup = findViewById(R.id.btn_signup);
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+            }
+        });
+
+        btnsignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://simponi2.ac.id"));
+                startActivity(intent);
+            }
+        });
+    }
+}
